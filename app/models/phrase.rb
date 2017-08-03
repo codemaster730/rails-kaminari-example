c82c898f59e63d44f90d7894dba0cee7dd0804c0
axiom88-guru <axiom88guru@gmail.com>
2017-08-02 19:25:24 -0700

    bug fixes

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,8 +1,9 @@
+require 'random_word_generator'
 class Phrase < ApplicationRecord
   # validates :content, uniqueness: true
 
-  def self.generate count_recods
-    count_recods.times do |i|
+  def self.generate count_records
+    count_records.times do |i|
       random_phase = RandomWordGenerator.composed(rand(5).to_i+3, rand(10).to_i+10)
       Phrase.create(
         content: random_phase
